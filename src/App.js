ffc694cc19f0c2233bfdae754bb539c12efc00ee
ceky <cekyy07@gmail.com>
2019-11-07 09:12:28 +0200

    Add all screens

--- src/App.js
+++ src/App.js
@@ -5,21 +5,34 @@ import Home from './components/Home';
 import BestMovies from './components/BestMovies';
 import BestDocumentaries from './components/BestDocumentaries';
 import './scss/App.scss';
+import BestBooks from './components/BestBooks';
+import BestSongs from './components/BestSongs';
+import BestYoutube from './components/BestYoutube';
+import BestTechnical from './components/BestTechnical';
 
 class App extends Component {
 
   render() {
+    
+new fullpage('#fullpage', {
+	anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+	menu: '#myMenu'
+});
+
     return (
       <ReactFullpage
         licenseKey = '^9hrscI$n7'
         scrollingSpeed = {1000}
-
         render={({ state, fullpageApi }) => {
           return (
             <ReactFullpage.Wrapper>
               <Home fullpageApi={fullpageApi}/>
               <BestMovies />
               <BestDocumentaries />
+              <BestBooks />
+              <BestSongs />
+              <BestYoutube />
+              <BestTechnical />
             </ReactFullpage.Wrapper>
           );
         }}
