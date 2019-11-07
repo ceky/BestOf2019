9d7bec14994eb44b4750fe79ba1abf7addcbd300
ceky <cekyy07@gmail.com>
2019-11-07 08:43:07 +0200

    Add bg images

---
+++ src/components/BestDocumentaries.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class BestDocumentaries extends Component {
+  
+  render() {
+    return (
+      <div className="section documentaries-container">
+        Best Documentaries
+      </div>
+    );
+  }
+}
+
+export default BestDocumentaries;
