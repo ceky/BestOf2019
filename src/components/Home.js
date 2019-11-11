527789d71b5b569d2f7c6b97046a445aa5038fde
ceky <cekyy07@gmail.com>
2019-11-12 00:16:22 +0200

    Add react-spring

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import {useSpring, animated} from 'react-spring';
 
 import './../scss/Home.scss';
 import logo from './../assets/logo.svg';
 
 const Home = ({fullpageApi}) => {
+
+  const fade = useSpring({
+    from: {
+      opacity: 0
+    },
+    opacity: 1
+  });
   
   return (
     <div className="section home-container">
       <div className="page-container">
-        <h1 className="title">Best of 2019</h1>
+        <animated.h1 className="title" style={fade}>Best of 2019</animated.h1>
         <div>This is the second year in a row I'm doing this. Having a look back to the past year</div>
 
         <img src={logo} className="App-logo" alt="logo" />
