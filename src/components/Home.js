fa697b929bd92def9b0c6f7601c7787149ba76e5
ceky <cekyy07@gmail.com>
2019-11-16 09:51:14 +0200

    Style the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useSpring, animated} from 'react-spring';
 
 import logo from './../assets/love.svg';
@@ -6,11 +6,13 @@ import './../scss/Home.scss';
 
 const Home = ({fullpageApi}) => {
 
-  const fade = useSpring({
+  const [isMouseOver, setMouseOver] = useState(false);
+
+  const scale = useSpring({
     from: {
-      opacity: 0
+      transform: isMouseOver ? `scale(1)` : `scale(1.1)`
     }, 
-    opacity: 1
+    transform: isMouseOver ? `scale(1.1)` : `scale(1)`
   });
   
   return (
@@ -19,16 +21,20 @@ const Home = ({fullpageApi}) => {
         <div className="page-container">
           <div className="title-container">
             <img className="title-background" alt="bg" src={logo}/>
-            <animated.h1 className="title" style={fade}>2019</animated.h1>
+            <h1 className="title">2019</h1>
           </div>
           
           <div className="main-container">
-            <div>This is the second year in a row I'm doing this. Having a look back to the past year, on the things I enjoyed the most in terms of content consumption, gives me a good feeling. It makes me feel grateful for the amount of awesome things that I came over. And I've spent so much time on some of them, with books like Flow or The Count of Monte Cristo, or songs that I had on repeat for so long. They are worth mentioning in a site just like this one, since these little things leave a mark on me.</div>
+            <div>This is the second year in a row I'm doing this. Having a look back to the past year, on the things I enjoyed the most in terms of content consumption, gives me a good feeling. I've spent so much time on some of them, with books like Flow or The Count of Monte Cristo, or songs that I had on repeat for so long. They are worth mentioning in a site just like this one, since these little things leave a mark on me.</div>
 
             <div>All in all it was a good year, having a curiosity for learning new things. I can't say I always had this high level of curiosity, but it's always rewarding when I'm experiencing it for longer periods of time. Having said that, let's start exploring the things that caught my attention in 2019!</div>
           </div>
 
-          <div onClick={() => fullpageApi.moveSectionDown()}>GO</div>
+          <animated.div className="start-button" 
+            style={scale}
+            onMouseOver={() => setMouseOver(true)} 
+            onMouseLeave={() => setMouseOver(false)}
+            onClick={() => fullpageApi.moveSectionDown()}>Start</animated.div>
         </div>
       </div>
     </div>
