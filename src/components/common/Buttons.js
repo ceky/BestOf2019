3fe4b342f23cf35d99c27d561e9f8dc4b0ea1890
ceky <cekyy07@gmail.com>
2019-11-29 17:02:49 +0200

    Fix initial button animation

--- src/components/common/Buttons.js
+++ src/components/common/Buttons.js
@@ -5,11 +5,21 @@ import './../../scss/components/Button.scss';
 
 const Button = ({label, onClick}) => {
 
-  const [isMouseOver, setMouseOver] = useState(false);
+  const [isMouseOver, setMouseOver] = useState('');
+
+  const getScaleFrom = () => {
+    // when isMouseOver is empty it means there hasn't been any interaction with the button yet
+    // and it should not start scaling down from 1.1 to 1 on the first load
+    if (isMouseOver === '' || isMouseOver === true) {
+      return 'scale(1)';
+    } else {
+      return 'scale(1.1)'
+    }
+  }
 
   const scale = useSpring({
     from: {
-      transform: isMouseOver ? `scale(1)` : `scale(1.1)`
+      transform: getScaleFrom()
     }, 
     transform: isMouseOver ? `scale(1.1)` : `scale(1)`,
     config: {
