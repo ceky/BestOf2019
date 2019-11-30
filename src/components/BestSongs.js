32b0a434695e52360a1683ba47afb9a18d4bc3ed
ceky <cekyy07@gmail.com>
2019-11-30 13:45:36 +0200

    Add new pages

--- src/components/BestSongs.js
+++ src/components/BestSongs.js
@@ -14,21 +14,21 @@ const BestSongs = () => {
     <div className="section songs-container">
       <div className="center">
         <div className="page-container">
-        <Title label="Best Songs"></Title>
+          <Title label="Best Songs"></Title>
 
-        <main className="page-content right-photo">
-          
-          <div>
-            <p>I just love listening to music. It's like therapy to me, it always helped me in difficult times. And this year I've discovered so many good songs and bands. I've also discovered some good concerts from NPR Music Tiny Desk. My favorite being by far the one from Mac Miller. Compared to past years I would say I've listened to less Romanian music however.</p>
+          <main className="page-content right-photo">
+            
+            <div>
+              <p>I just love listening to music. It's like therapy to me, it always helped me in difficult times. And this year I've discovered so many good songs and bands. I've also discovered some good concerts from NPR Music Tiny Desk. My favorite being by far the one from Mac Miller. Compared to past years I would say I've listened to less Romanian music however.</p>
 
-            <p>Listening to so many good songs I just couldn't limit my selections to only 5 picks as I did last year.</p>
-          </div>
+              <p>Listening to so many good songs I just couldn't limit my selections to only 5 picks as I did last year.</p>
+            </div>
 
-          <MusicIcon />
-          
-        </main>     
+            <MusicIcon />
+            
+          </main>     
 
-        <Button label='See Songs' onClick={onClickSeeSongs} />
+          <Button label='See Songs' onClick={onClickSeeSongs} />
         </div>
       </div>
     </div>
--- src/components/BestTechnical.js
+++ src/components/BestTechnical.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
+import Button from './common/Buttons';
+import Title from './common/Title';
+import MusicIcon from './icons/MusicIcon';
+
 const BestTechnical = () => {
   
+  const onClickSeeTechnical = () => {
+    console.log('new test');
+  }
+
   return (
     <div className="section technical-container">
-      <div className="page-container">
-      Best Technical
+      <div className="center">
+        <div className="page-container">
+          <Title label="Best Technical"></Title>
+
+          <main className="page-content right-photo">
+            
+            <div>
+              <p>I just love listening to music. It's like therapy to me, it always helped me in difficult times. And this year I've discovered so many good songs and bands. I've also discovered some good concerts from NPR Music Tiny Desk. My favorite being by far the one from Mac Miller. Compared to past years I would say I've listened to less Romanian music however.</p>
+
+              <p>Listening to so many good songs I just couldn't limit my selections to only 5 picks as I did last year.</p>
+            </div>
+
+            <MusicIcon />
+            
+          </main>     
+
+          <Button label='See Technical' onClick={onClickSeeTechnical} />
+        </div>
       </div>
     </div>
   );
