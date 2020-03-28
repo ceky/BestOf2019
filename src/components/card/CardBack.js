6c89ffe6d952e378b4e633b88d9abee50b41ed25
ceky <cekyy07@gmail.com>
2020-03-28 23:14:57 +0200

    Add Card component

---
+++ src/components/card/CardBack.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class CardBack extends Component {
+  render() {
+    return(
+      <div className='card-side side-back'>
+        <h1>Let's get in touch!</h1>
+      </div>
+    )
+  }
+}
+
+export default CardBack;
---
+++ src/components/card/CardFront.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class CardFront extends Component {
+  render() {
+    return(
+      <div className='card-side side-front'>
+        <h2>Czech based</h2>
+      </div>
+    )
+  }
+}
+
+export default CardFront;
