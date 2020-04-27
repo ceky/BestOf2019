2f05291d3df427a45c4587bbb1cc3612de2711e0
ceky <cekyy07@gmail.com>
2020-04-27 22:07:49 +0300

    Start mobile version

--- src/components/TransitionScreen.js
+++ src/components/TransitionScreen.js
@@ -9,7 +9,7 @@ const TransitionScreen = ({category, isOpen, activeIndex, onClose}) => {
 
   useEffect(() => {
     if (isOpen) {
-      // document.getElementsByTagName('html')[0].style.overflow = 'auto';
+      document.getElementById('transition-screen').scrollTop = 0;
     }
   }, [isOpen]);
 
@@ -20,7 +20,6 @@ const TransitionScreen = ({category, isOpen, activeIndex, onClose}) => {
   const onClickClose = () => {
     onClose();
 
-    // document.getElementsByTagName('html')[0].style.overflow = 'hidden';
     window.fullpageApi.silentMoveTo(activeIndex);
   }
 
@@ -33,24 +32,43 @@ const TransitionScreen = ({category, isOpen, activeIndex, onClose}) => {
       style={{
         transform: y.interpolate(y => `translate3d(0, ${y}%, 0)`)
       }}
-      className="transition-screen">
+      className="transition-screen" id="transition-screen">
 
       <img className="close-btn" alt="close" src={closeBtn} onClick={() => onClickClose()}/>
 
       <p className="transition-title">Best {category}</p>
 
-      <ul className="list">
+      <div className="desktop">
+        <ul className="list">
 
-        { bestThings[category].map((item, key) =>
-          (<li key={key} className="list-item" onClick={() => onOpenLink(item.link)}>
-            <img src={item.thumbnail} alt="thumb"></img>
-            <div className="text-container">
-              <p className="title">{item.title}</p>
-              <p className="description">{item.description}</p>
-            </div>            
-          </li>)
-        )}
-      </ul>
+          { bestThings[category].map((item, key) =>
+            (<li key={key} className="list-item" onClick={() => onOpenLink(item.link)}>
+              <img src={item.thumbnail} alt="thumb"></img>
+              <div className="text-container">
+                <p className="title">{item.title}</p>
+                <p className="description">{item.description}</p>
+              </div>            
+            </li>)
+          )}
+        </ul>
+      </div>
+
+      <div className="mobile">
+        <ul className="list">
+
+          { bestThings[category].map((item, key) =>
+            (<li key={key} className="list-item" onClick={() => onOpenLink(item.link)}>
+              <img src={item.thumbnail} alt="thumb"></img>
+              <div className="text-container">
+                <p className="title">{item.title}</p>
+                <p className="description">{item.description}</p>
+              </div>            
+            </li>)
+          )}
+        </ul>
+      </div>
+
+      
       
     </animated.div>
   );
